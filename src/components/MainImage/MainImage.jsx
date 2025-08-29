a7dc13a4c4f316d63a4730e7de9ac5f606b31bdd
Adrian-Koh <adriankoh216@yahoo.com>
2025-08-29 18:01:11 +0800

    Calculate clicked image coordinates correctly
    Ensure the position text above image stays the same height

--- src/components/MainImage/MainImage.jsx
+++ src/components/MainImage/MainImage.jsx
@@ -3,40 +3,58 @@ import styles from "./MainImage.module.css";
 
 const MainImage = () => {
   const [position, setPosition] = useState("");
-  const [topLeftX, setTopLeftX] = useState(-1);
-  const [topLeftY, setTopLeftY] = useState(-1);
+  const [containerX, setContainerX] = useState(-1);
+  const [containerY, setContainerY] = useState(-1);
+  const [imageX, setImageX] = useState(-1);
+  const [imageY, setImageY] = useState(-1);
   const [circleX, setCircleX] = useState(-1);
   const [circleY, setCircleY] = useState(-1);
   const containerRef = useRef(null);
+  const imgRef = useRef(null);
 
   useEffect(() => {
     if (containerRef.current) {
       const rect = containerRef.current.getBoundingClientRect();
       console.log(`Container top-left corner: (${rect.x}, ${rect.y})`);
-      setTopLeftX(rect.x);
-      setTopLeftY(rect.y);
+      setContainerX(rect.x);
+      setContainerY(rect.y);
     }
   }, []);
+
+  function initializeImageCoords() {
+    if (imgRef.current) {
+      const rect = imgRef.current.getBoundingClientRect();
+      console.log(`Image top-left corner: (${rect.x}, ${rect.y})`);
+      setImageX(rect.x);
+      setImageY(rect.y);
+    }
+  }
 
   function handleImageClick(e) {
     const posX = e.clientX;
     const posY = e.clientY;
 
-    setPosition(`Clicked on viewport coordinates: (${posX}, ${posY})`);
+    setPosition(
+      `viewport: (${posX}, ${posY}), image: (${posX - imageX}, ${
+        posY - imageY
+      })`
+    );
 
     const CIRCLE_RADIUS = 25;
-    setCircleX(posX - topLeftX - CIRCLE_RADIUS);
-    setCircleY(posY - topLeftY - CIRCLE_RADIUS);
+    setCircleX(posX - containerX - CIRCLE_RADIUS);
+    setCircleY(posY - containerY - CIRCLE_RADIUS);
   }
 
   return (
     <div className={styles.container} ref={containerRef}>
-      <p className={styles.position}>{position}</p>
+      <p className={styles.position}>Clicked coords: {position}</p>
       <img
         className={styles.mainImage}
         src="/characters.png"
         alt="All Naruto characters"
         onClick={handleImageClick}
+        ref={imgRef}
+        onLoad={initializeImageCoords}
       />
       <div
         className={styles.selectedCircle}
